87813942673a152e95cbea25db2264f3dac6b056
Rohit Shrestha <106756449+RohitSth@users.noreply.github.com>
2024-03-11 15:39:51 +0545

    es6 modules export and import

---
+++ essential_js/modules/es6Modules.js
@@ -0,0 +1,19 @@
+// modules
+
+// example 1
+
+export const PI = 3.14159;
+
+export function getCircumference(radius) {
+  return 2 * PI * radius;
+}
+
+export function getArea(radius) {
+  return PI * radius * radius;
+}
+
+export function getVolume(radius) {
+  return (4 / 3) * PI * radius * radius * radius;
+}
+
+// go to es6-modules.html to see the result
---
+++ essential_js/modules/index.js
@@ -0,0 +1,6 @@
+import { PI, getCircumference, getVolume, getArea } from "./es6Modules.js";
+
+console.log(PI);
+console.log(getCircumference(5));
+console.log(getArea(5));
+console.log(getVolume(5));

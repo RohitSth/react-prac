62a555a6a09e9311f5d715f61f4e4113574733df
Rohit Shrestha <106756449+RohitSth@users.noreply.github.com>
2024-03-11 15:26:19 +0545

    learned template-literals

---
+++ essential_js/template-literals.js
@@ -0,0 +1,11 @@
+// Template liters = are literals delimited with backtick (`) characters
+
+// Example 1
+
+let n1 = 10;
+let n2 = 20;
+
+console.log(n1 + n2);
+
+// using literals
+console.log(`${n1} + ${n2} = ${n1 + n2}`);
